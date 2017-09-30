c144a83c66234e2c9d1deb528fa614c55ce8fb61
fotanus <fotanus@gmail.com>
2017-09-30 16:34:35 -0300

    interface draft

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Todo extends Component {
+  render () {
+    return (
+      <div>
+        <input type="checkbox" />
+        <input type="text" />
+        <textarea />
+      </div>
+    )
+  }
+}
+
+class Menu extends Component {
+  render () {
+    return (
+      <div>
+        <button>New task</button>
+        <button>Delete selected</button>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Todo/>
+        <Menu/>
       </div>
     );
   }
